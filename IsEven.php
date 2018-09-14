04a7ed3c950a8dff025baa802da6ef5e42895cf1
Unknown <jhample323@west-mec.org>
2018-09-13 18:23:23 -0700

    code boyo
    Did some more excercise

--- IsEven.php
+++ IsEven.php
@@ -18,7 +18,16 @@
 <body>
     <h2>Is Even</h2>
     <?php
-    $numbersArray = array()
+    // Array that creates random numbers
+    $numbersArray = array(rand(1,10), rand(1,10), rand(1,10), rand(1,10), rand(1,10), rand(1,10), rand(1,10), rand(1,10), rand(1,10), rand(1,10));
+    $x = 0;
+    $y = 1;
+    // Checks for even numbers
+    $returnValue = $numbersArray % $x;
+    echo '<p>$returnValue is an even number</p>';
+    // Checks for odd numbers
+    $returnValue = $numbersArray % $y;
+    echo '<p>$returnValue is an odd number</p>';
     ?>
 </body>
 
